8e70141237f471af7731103c62708c86a13a8abe
JonatanDrysen <jojodrysen@gmail.com>
2024-02-23 13:02:32 +0100

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from "./Components/Header";
+import { Nav } from "./Components/Nav";
 
 export function App() {
   return (
     <>
+      <Nav />
       <main>
         <Header text={"Hot"} />      
-
+        
       </main>
     </>
   );
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+export function Nav() {
+    return (
+        <>
+            <section>
+                <ul>
+                    <li>
+                        <a href="#">Hot</a>
+                    </li>
+                    <li>
+                        <a href="#">Juicy</a>
+                    </li>
+                    <li>
+                        <a href="#">Cosy</a>
+                    </li>
+                </ul>
+            </section>
+        </>
+    )
+}
